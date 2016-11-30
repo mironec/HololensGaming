841fc789b31d9c385da512e242ba739b9d6a23eb
Miron Zelina <mironzelinajr@gmail.com>
2016-11-30 22:57:08 +0000

    Initial commit for RotationTranslationDemo

---
+++ RotationTranslationDemo/Assets/Scripts/GestureAction.cs
@@ -0,0 +1,154 @@
+﻿using UnityEngine;
+using HoloToolkit.Unity;
+
+/// <summary>
+/// GestureAction performs custom actions based on
+/// which gesture is being performed.
+/// </summary>
+public class GestureAction : MonoBehaviour
+{
+    [Tooltip("Rotation max speed controls amount of rotation.")]
+    public float RotationSensitivity = 10.0f;
+
+    [Tooltip("Translation sensitivity controls the speed of the translation.")]
+    public float TranslationSensitivity = 10.0f;
+
+    private bool doingTranslation = false;
+    private bool doingRotation = false;
+    private bool beingManipulated = false;
+    private Vector3 initialPosition;
+
+    void Start()
+    {
+        /*MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        CombineInstance[] combine = new CombineInstance[meshFilters.Length];
+        int i = 0;
+        while (i < meshFilters.Length)
+        {
+            combine[i].mesh = meshFilters[i].sharedMesh;
+            combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
+            meshFilters[i].gameObject.SetActive(false);
+            i++;
+        }
+        transform.GetComponent<MeshFilter>().mesh = new Mesh();
+        transform.GetComponent<MeshFilter>().mesh.CombineMeshes(combine);
+        transform.gameObject.SetActive(true);
+        transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);*/
+        initialPosition = transform.position;
+    }
+
+    void Awake()
+    {
+        foreach (Transform c in transform)
+        {
+            if (c.gameObject.GetComponent<GestureHelpResponder>() == null)
+                c.gameObject.AddComponent<GestureHelpResponder>();
+        }
+
+        CustomGestureManager.Instance.OnManipulationStarted += PerformManipulationStart;
+        CustomGestureManager.Instance.OnManipulationCompleted += PerformManipulationEnd;
+        CustomGestureManager.Instance.OnManipulationCanceled += PerformManipulationEnd;
+    }
+
+    void OnSelect()
+    {
+        doingTranslation = true;
+        doingRotation = false;
+        /*isSelected = true;
+        foreach(Transform c in transform)
+        {
+            c.gameObject.GetComponent<Renderer>().material.color = new Color(0.8f, 0.8f, 1.0f);
+        }*/
+    }
+
+    void OnDoubleTap()
+    {
+        Debug.Log("Double tap A.");
+        doingRotation = true;
+        doingTranslation = false;
+    }
+
+    void OnReleased()
+    {
+        /*isSelected = false;
+        foreach (Transform c in transform)
+        {
+            c.gameObject.GetComponent<Renderer>().material.color = new Color(1.0f, 1.0f, 1.0f);
+        }*/
+    }
+
+    private void snapToGrid() {
+        transform.eulerAngles = new Vector3(roundAngle(transform.eulerAngles.x), roundAngle(transform.eulerAngles.y), roundAngle(transform.eulerAngles.z));
+    }
+
+    private float roundAngle(float angle)
+    {
+        angle = Mathf.Round(angle / 90.0f);
+        if (angle == 4.0f) angle = 0.0f;
+        if (angle == -4.0f) angle = 0.0f;
+
+        return (angle * 90.0f);
+    }
+
+    void Update()
+    {
+        if (doingRotation)
+            PerformRotation();
+        if (doingTranslation)
+            PerformTranslation();
+    }
+
+    private void PerformRotation()
+    {
+        if (IsThisFocusedByGaze() && CustomGestureManager.Instance.ManipulationInProgress)
+        {
+            float rotationAroundY = CustomGestureManager.Instance.ManipulationOffset.x * RotationSensitivity;
+            float rotationAroundZ = CustomGestureManager.Instance.ManipulationOffset.y * RotationSensitivity;
+            float rotationAroundX = CustomGestureManager.Instance.ManipulationOffset.z * RotationSensitivity;
+
+            transform.Rotate(new Vector3(rotationAroundX, rotationAroundY, rotationAroundZ));
+        }
+    }
+
+    private bool IsThisFocusedByGaze() {
+        return CustomGestureManager.Instance.FocusedObject != null && (CustomGestureManager.Instance.FocusedObject == gameObject || CustomGestureManager.Instance.FocusedObject.transform.parent.gameObject == gameObject);
+    }
+
+    public void PerformManipulationStart(UnityEngine.VR.WSA.Input.InteractionSourceKind sourceKind)
+    {
+        if (IsThisFocusedByGaze()) {
+            initialPosition = transform.position;
+            beingManipulated = true;
+        }
+    }
+
+    public void PerformManipulationEnd(UnityEngine.VR.WSA.Input.InteractionSourceKind sourceKind)
+    {
+        beingManipulated = false;
+        snapToGrid();
+    }
+
+    private void PerformTranslation()
+    {
+        if (beingManipulated)
+        {
+            transform.position = initialPosition + CustomGestureManager.Instance.ManipulationOffset * TranslationSensitivity;
+        }
+    }
+
+    private class GestureHelpResponder : MonoBehaviour
+    {
+        void OnSelect()
+        {
+            transform.parent.SendMessage("OnSelect");
+        }
+        void OnReleased()
+        {
+            transform.parent.SendMessage("OnReleased");
+        }
+        void OnDoubleTap()
+        {
+            transform.parent.SendMessage("OnDoubleTap");
+        }
+    }
+}
---
+++ RotationTranslationDemo/Assets/Scripts/VisualEffects.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class VisualEffects : MonoBehaviour {
+
+    public float ballsPerSecond = 2.5f;
+    public GameObject ballPrefab;
+    private float timeSinceLastBall = 0.0f;
+    private float secondsPerBall;
+
+	// Use this for initialization
+	void Start () {
+        secondsPerBall = 1.0f / ballsPerSecond;
+    }
+	
+	void FixedUpdate () {
+        timeSinceLastBall += Time.fixedDeltaTime;
+        while (timeSinceLastBall > secondsPerBall) {
+            GameObject ball = Instantiate(ballPrefab);
+            List<GameObject> ceilings = HoloToolkit.Unity.SurfaceMeshesToPlanes.Instance.GetActivePlanes(HoloToolkit.Unity.PlaneTypes.Ceiling);
+            GameObject ceiling = ceilings[Random.Range(0, ceilings.Count)];
+            if (ceilings.Count <= 0)
+            {
+                Debug.Log("No ceiling, returning.");
+                timeSinceLastBall = 0.0f;
+                return;
+            }
+            Mesh planeMesh = ceiling.GetComponent<MeshFilter>().mesh;
+            Bounds bounds = planeMesh.bounds;
+            float minX = ceiling.transform.position.x - ceiling.transform.localScale.x * bounds.size.x * 0.5f;
+            float minZ = ceiling.transform.position.z - ceiling.transform.localScale.z * bounds.size.z * 0.5f;
+            ball.transform.position = new Vector3(Random.Range(minX, -minX),
+                HoloToolkit.Unity.SurfaceMeshesToPlanes.Instance.CeilingYPosition - ball.GetComponent<MeshFilter>().mesh.bounds.size.y * ball.transform.localScale.y * 0.5f,
+                Random.Range(minZ, -minZ));
+            timeSinceLastBall -= secondsPerBall;
+        }
+	}
+}
