e3f571eb8e3d9d5b2061f156610ce47474340a47
Miron Zelina <mironzelinajr@gmail.com>
2016-12-12 20:39:54 +0000

    Initial commit for HoloAsteroids demo
    HoloAsteroids is a simple game where the player has to dodge asteroids
    coming towards them, and they gain score for an unbroken streak of dodges.

---
+++ HoloAsteroids/Assets/Scripts/Asteroid.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Asteroid : MonoBehaviour {
+
+    public GameObject playerRef;
+    public float gazeDestroyTime;
+
+    private float gazeTime;
+
+	void Start () {
+	    
+	}
+
+    void Update() {
+        Renderer r = GetComponent<Renderer>();
+        Color c;
+        c.r = 0.8f;
+        c.g = 0.8f - (gazeTime / gazeDestroyTime) * 0.8f;
+        c.b = 0.8f - (gazeTime / gazeDestroyTime) * 0.8f;
+        c.a = 1.0f;
+        r.material.color = c;
+    }
+	
+	void FixedUpdate () {
+        if (Vector3.Distance(transform.position, playerRef.transform.position) > 25.0f) {
+            Destroy(gameObject);
+        }
+        if (gazeTime > gazeDestroyTime)
+            Destroy(gameObject);
+        gazeTime -= Time.deltaTime / 5.0f;
+        if (gazeTime < 0.0f) {
+            gazeTime = 0.0f;
+        }
+	}
+
+    public void Gaze(float time) {
+        gazeTime += time;
+    }
+}
---
+++ HoloAsteroids/Assets/Scripts/GameSystem.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class GameSystem : MonoBehaviour {
+
+    public Asteroid originalAsteroid;
+    public GameObject player;
+    public float inverseAsteroidChance;
+    public Text scoreText;
+
+    private float lastAsteroid;
+    private ArrayList asteroids;
+    private float score;
+    private float elapsedTime;
+
+    void Start()
+    {
+        lastAsteroid = Time.fixedTime;
+        asteroids = new ArrayList();
+        score = 0.0f;
+        elapsedTime = 0.0f;
+    }
+
+    void UpdateScoreText() {
+        scoreText.text = "Score: " + ((int)score).ToString();
+    }
+
+    void Update() {
+        UpdateScoreText();
+    }
+
+    void FixedUpdate()
+    {
+        score += Time.deltaTime * elapsedTime;
+        elapsedTime += Time.deltaTime;
+        float currentTime = Time.fixedTime;
+        if (Random.Range(0.0f, inverseAsteroidChance) * Time.deltaTime < currentTime - lastAsteroid)
+        {
+            Vector3 pos = player.transform.forward * 10.0f;
+            pos = Vector3.RotateTowards(pos, Random.rotation * player.transform.forward, 0.25f, 10000.0f);
+            Asteroid newAsteroid = Instantiate(originalAsteroid, pos, Quaternion.Inverse(player.transform.rotation)) as Asteroid;
+            lastAsteroid = currentTime;
+            asteroids.Add(newAsteroid);
+            newAsteroid.transform.localScale *= Random.Range(0.8f, 1.2f);
+            newAsteroid.playerRef = player;
+            Rigidbody body = newAsteroid.GetComponent<Rigidbody>();
+            body.AddForce( (player.transform.position - pos) * 10.0f );
+        }
+    }
+
+    void SetScore(float score) {
+        this.score = score;
+    }
+
+    public void PlayerHitByAsteroid() {
+        score = 0.0f;
+        elapsedTime = 0.0f;
+
+        foreach (Asteroid a in asteroids) {
+            if (a == null) continue;
+            Destroy(a.gameObject);
+        }
+        asteroids = new ArrayList();
+        lastAsteroid = Time.fixedTime;
+    }
+}
