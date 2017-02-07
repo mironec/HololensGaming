ccbebc798d549f318c6810462827849598eddfbd
Tilman Schmidt <AccessKeyMaster@gmx.net>
2017-02-07 19:15:17 +0000

    HoloBall; Added goal trigger and ball detection
    Also configured the hololens marker tracking plugin correctly for debug
    builds (they however don't work with the current script, I suspect
    marshalling issues but don't know specifics)

---
+++ HoloBallGame/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+    private static GameManager instance;
+    public static GameManager obj
+    {
+        get { return instance; }
+        private set { }
+    }
+
+    public GameObject ball;
+
+	// Use this for initialization
+	void Start () {
+        instance = this;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
