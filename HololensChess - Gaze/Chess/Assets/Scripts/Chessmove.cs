34100c10ca320fc1372cb245edcd38b1b4a51cff
Mehul <mehul1996@hotmail.com>
2017-02-09 13:48:19 +0000

    adding gaze feature to game please test

---
+++ HololensChess - Gaze/Chess/Assets/Scripts/Chessmove.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public abstract class Chessmove : MonoBehaviour
+{
+    public int CurrentX { set; get; }
+    public int CurrentY { set; get; }
+    public bool isWhite;
+
+    public void SetPosition(int x, int y)
+    {
+        CurrentX = x;
+        CurrentY = y;
+    }
+
+    public virtual bool[,] PossibleMove()
+    {
+        return new bool[8,8]; 
+
+    }
+
+}
