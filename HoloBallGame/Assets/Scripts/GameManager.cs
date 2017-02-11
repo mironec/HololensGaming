e0ab6b77823b8018c2c8ea8eea3cb7bc0bf9a3be
Tilman Schmidt <AccessKeyMaster@gmx.net>
2017-02-11 18:53:53 +0000

    Added basic start/finish/reset loop to HoloBall
    Extended the GameManager object to broadcast events for others to access
    the ball game object, and added an event to the GoalTrigger for the
    reverse notification of finishing a level. Also added
    pause/unpause/reset functions and keyboard triggers for them to the
    GameManager.

--- HoloBallGame/Assets/Scripts/GameManager.cs
+++ HoloBallGame/Assets/Scripts/GameManager.cs
@@ -1,24 +1,80 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour {
-    private static GameManager instance;
-    public static GameManager obj
-    {
-        get { return instance; }
-        private set { }
-    }
+    public static event Action<GameObject> on_ball_set;
 
     public GameObject ball;
+    Vector3 ball_start_pos;
+    Quaternion ball_start_rot;
+
+    public GoalTrigger goal_trigger;
+
+    bool paused;
+    bool level_complete;
 
 	// Use this for initialization
 	void Start () {
-        instance = this;
+        ball_start_pos = ball.transform.position;
+        ball_start_rot = ball.transform.rotation;
+
+        StartCoroutine(emit_start_events());
+        goal_trigger.on_goal_reached += on_goal_reached;
 	}
+
+        //Delaying events that are emitted on start by one frame so we can be sure everyone has subscribed during Start() calls
+    private IEnumerator emit_start_events() {
+        yield return null;
+        on_ball_set.Invoke(ball);
+    }
 	
 	// Update is called once per frame
 	void Update () {
-		
+		if(Input.GetKeyDown(KeyCode.Space)) {
+            if(level_complete) {
+                reset_game();
+                unpause_game();
+            }
+            else {
+                if (paused) {
+                    unpause_game();
+                }
+                else {
+                    pause_game();
+                }
+            }
+        }
+
+        if(Input.GetKeyDown(KeyCode.R)) {
+            reset_game();
+            pause_game();
+        }
 	}
+
+    void on_goal_reached() {
+        pause_game();
+        level_complete = true;
+        Debug.Log("Goal reached!");
+    }
+
+    void pause_game() {
+        Time.timeScale = 0.0f;
+        paused = true;
+    }
+
+    void unpause_game() {
+        Time.timeScale = 1.0f;
+        paused = false;
+    }
+
+    void reset_game() {
+        level_complete = false;
+        ball.transform.position = ball_start_pos;
+        ball.transform.rotation = ball_start_rot;
+        Rigidbody ball_rb = ball.GetComponent<Rigidbody>();
+        ball_rb.velocity = Vector3.zero;
+        ball_rb.angularVelocity = Vector3.zero;
+    }
 }
--- HoloBallGame/Assets/Scripts/GoalTrigger.cs
+++ HoloBallGame/Assets/Scripts/GoalTrigger.cs
@@ -1,24 +1,34 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class GoalTrigger : MonoBehaviour {
+    public event Action on_goal_reached;
+
+    Collider ball_collider;
 
 	// Use this for initialization
 	void Start () {
-		
+        GameManager.on_ball_set += on_ball_set;
 	}
-	
-	// Update is called once per frame
-	void Update () {
+
+    private void OnDestroy() {
+        GameManager.on_ball_set -= on_ball_set;
+    }
+
+    private void on_ball_set(GameObject _ball) {
+        ball_collider = _ball.GetComponent<Collider>();
+    }
+
+    // Update is called once per frame
+    void Update () {
 		
 	}
 
-    private void OnTriggerEnter(Collider other)
-    {
-        if(Collider.Equals(GameManager.obj.ball.GetComponent<Collider>(), other))
-        {
-            Debug.Log("Ball entered goal");
+    private void OnTriggerEnter(Collider other) {
+        if(Collider.Equals(ball_collider, other)) {
+            on_goal_reached.Invoke();
         }
     }
 }
