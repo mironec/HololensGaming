3f425ff4a3039fd35da83d2dd8c2f3f3c53a6f71
Miron Zelina <mironzelinajr@gmail.com>
2017-02-14 14:06:48 +0000

    Minimized HoloChess
    Fixed HololensChess works on the Hololens with tapping and has a new
    structure, with a part of the HoloToolkit.

---
+++ HololensChess - Fixed/Chess/Assets/Scripts/Chessmove.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using HoloToolkit.Unity.InputModule;
+using UnityEngine;
+
+public abstract class Chessmove : MonoBehaviour
+{
+    public int CurrentX { set; get; }
+    public int CurrentY { set; get; }
+    public bool isWhite;
+
+    public void SetPosition(int x, int y)
+    {
+        CurrentX = x;
+        CurrentY = y;
+    }
+
+    public virtual bool[,] PossibleMove()
+    {
+        return new bool[8,8]; 
+    }
+}
