2b8768b9f724ed195e8ecedb6661bd86040aeee3
Miron Zelina <mironzelinajr@gmail.com>
2017-02-16 22:44:22 +0000

    Add speed boost prefab

---
+++ HoloBallGame/Assets/Scripts/SpeedBoost.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpeedBoost : MonoBehaviour {
+
+    public float speedBoostStrength = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
+        if (rigidbody == null) return;
+        rigidbody.velocity += transform.forward * speedBoostStrength;
+        Debug.Log(other.gameObject + " Entered!");
+    }
+}
