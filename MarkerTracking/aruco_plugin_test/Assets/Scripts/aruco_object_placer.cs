4524ac57c05cd99fea0118250cdabe661a9625f5
Tilman Schmidt <AccessKeyMaster@gmx.net>
2017-02-17 17:36:45 +0000

    Moved tracking to its own class
    All of the library functions are now bundled in one C# class, providing
    the init/destroy/detect_markers functionality via static methods.
    As a consequence, this functionality got moved out of the aruco_tracker
    script (now aruco_object_placer), which will get further refactored or
    rewritten to create full untiy integration that will be the final
    tracking library.

--- MarkerTracking/aruco_plugin_test/Assets/Scripts/aruco_object_placer.cs
+++ MarkerTracking/aruco_plugin_test/Assets/Scripts/aruco_object_placer.cs
@@ -5,7 +5,7 @@ using UnityEngine.UI;
 using System.Runtime.InteropServices;
 using System;
 
-public class aruco_tracker : MonoBehaviour {
+public class aruco_object_placer : MonoBehaviour {
     public int deviceNumber;
     public Texture2D test_img;
     public bool use_test_img = false;
@@ -28,23 +28,6 @@ public class aruco_tracker : MonoBehaviour {
     private int cam_height = -1;
 
     private float webcam_fov;
-
-    [DllImport("aruco_plugin")]
-    public static extern void init(int width, int height, float marker_size, IntPtr camera_params, int size_reduce);
-
-    [DllImport("aruco_plugin")]
-    public static extern int detect_markers(IntPtr unity_img, ref int marker_count, ref IntPtr out_ids, ref IntPtr out_corners, ref IntPtr out_rvecs, ref IntPtr out_tvecs);
-
-    [DllImport("aruco_plugin")]
-    public static extern void set_debug_cb(IntPtr ptr);
-
-    [DllImport("aruco_plugin")]
-    public static extern void destroy();
-
-    [UnmanagedFunctionPointer(CallingConvention.Cdecl)]
-    public delegate void PrintDelegate(string str);
-    
-    private bool dll_inited = false;
 
     private Vector2 resolution;
     
@@ -87,27 +70,18 @@ public class aruco_tracker : MonoBehaviour {
             }
         }
 
-        if(cam_width != -1)
-        {
+        if (cam_width != -1) {
             init_camera_params();
-
-            GCHandle params_handle = GCHandle.Alloc(camera_params, GCHandleType.Pinned);
-            init(cam_width, cam_height, marker_size, params_handle.AddrOfPinnedObject(), size_reduce);
-            params_handle.Free();
-            dll_inited = true;
-        }
-
-        PrintDelegate plugin_delegate = new PrintDelegate(PluginDebugLog);
-        IntPtr delegate_ptr = Marshal.GetFunctionPointerForDelegate(plugin_delegate);
-        set_debug_cb(delegate_ptr);
+            ArucoTracking.init(cam_width, cam_height, marker_size, camera_params, size_reduce);
+        }        
 
         quad_instances = new List<GameObject>();
 
         //Taken from this stackoverflow answer:
         // http://stackoverflow.com/questions/36561593/opencv-rotation-rodrigues-and-translation-vectors-for-positioning-3d-object-in
-        float focal_y = camera_params[1];
-        webcam_fov = 2.0f * Mathf.Atan(0.5f * cam_height / focal_y) * Mathf.Rad2Deg;
-        cam.aspect = (float)cam_width / cam_height;
+        //float focal_y = camera_params[1];
+        //webcam_fov = 2.0f * Mathf.Atan(0.5f * cam_height / focal_y) * Mathf.Rad2Deg;
+        //cam.aspect = (float)cam_width / cam_height;
     }
 
     void init_camera_params()
@@ -156,78 +130,48 @@ public class aruco_tracker : MonoBehaviour {
 
     void OnDestroy()
     {
-        if(dll_inited) destroy();
+        ArucoTracking.destroy();
     }
     
     // Update is called once per frame
     void Update () {
-        if (!dll_inited) return;
+        if (!ArucoTracking.lib_inited) return;
+
         if (!use_test_img)
         {
             _webcamTexture.GetPixels32(colors);
         }
         
-        //IntPtr imageHandle = getImageHandle(colors);
-        GCHandle imageHandle = GCHandle.Alloc(colors, GCHandleType.Pinned);
+        ArucoTracking.detect_markers(colors);
 
-        int marker_count = 0;
-        IntPtr out_ids = IntPtr.Zero;
-        IntPtr out_corners = IntPtr.Zero;
-        IntPtr out_rvecs = IntPtr.Zero;
-        IntPtr out_tvecs = IntPtr.Zero;
-
-            //This call may trigger a stack overflow exception from within the dll if the application is close to the memory limit of the HoloLens. 
-            //This isn't really a stack overflow, but instead an exception thrown from with OpenCV when failing to allocate memory.
-            //This can happen if the unity application allocates large amounts of memory over time, in which case the GC may wait until the heap reaches the maximum memory available to collect and free memory.
-            //In that case, this unmanaged call may run before space is freed up by the GC, so internal allocations done by OpenCV hit the memory limit, throwing the exception.
-            //The easiest way to prevent this is to never hit the memory limit, or never allocate so fast that the GC will let the heap approach maximum size.
-            //However, if necessary it is possible to use GC.AddMemoryPressure to make it aware of an approximate amount of memory used by the dll (probably 1 - 2 times the image size in bytes, as it creates some copies of it for color conversion)
-            //I tried this, but it seems to just cause the GC to collect every frame, which is terrible for performance. Because of that it's not implemented here.
-            //:todo: Look into a way to detect that the application is close to the maximum memory allowed (through a C# call or possibly a hardcoded value adjusted for the HoloLens (on emulator: 964 MB +- 4 MB maybe)) and force a GC.collect call if we're close to it.
-        detect_markers(imageHandle.AddrOfPinnedObject(), ref marker_count, ref out_ids, ref out_corners, ref out_rvecs, ref out_tvecs);
-
-        imageHandle.Free();
         //Add/remove quads to match how many we saw
-        if (quad_instances.Count > marker_count)
+        if (quad_instances.Count > ArucoTracking.marker_count)
         {
             //Clear out any instances we don't need anymore
-            for (int i = quad_instances.Count - 1; i >= marker_count; i--)
+            for (int i = quad_instances.Count - 1; i >= ArucoTracking.marker_count; i--)
             {
                 GameObject.Destroy(quad_instances[i]);
                 quad_instances.RemoveAt(i);
             }
         }
-        else if (marker_count > quad_instances.Count)
+        else if (ArucoTracking.marker_count > quad_instances.Count)
         {
-            int to_add = marker_count - quad_instances.Count;
+            int to_add = ArucoTracking.marker_count - quad_instances.Count;
             for (int i = 0; i < to_add; i++)
             {
                 quad_instances.Add(make_marker_obj());
             }
         }
 
-        if (marker_count > 0)
+        if (ArucoTracking.marker_count > 0)
         {
-                //Copy over data from plugin side to c# managed arrays
-            int[] ids = new int[marker_count];
-            Marshal.Copy(out_ids, ids, 0, marker_count);
-
-            float[] corners = new float[marker_count * 8];
-            Marshal.Copy(out_corners, corners, 0, marker_count * 8);
-
-            double[] rvecs = new double[marker_count * 3];
-            Marshal.Copy(out_rvecs, rvecs, 0, marker_count * 3);
-                
-            double[] tvecs = new double[marker_count * 3];
-            Marshal.Copy(out_tvecs, tvecs, 0, marker_count * 3);
-            
             // Extract vectors from data and apply transformation to objects
-            for (int i = 0; i < marker_count; i++)
+            for (int i = 0; i < ArucoTracking.marker_count; i++)
             {
-                Vector3 tvec = new Vector3((float)tvecs[i * 3], (float)tvecs[i * 3 + 1], (float)tvecs[i * 3 + 2]);
+                Vector3 tvec = new Vector3((float)ArucoTracking.tvecs[i * 3], (float)ArucoTracking.tvecs[i * 3 + 1], (float)ArucoTracking.tvecs[i * 3 + 2]);
                 quad_instances[i].transform.localPosition = tvec;
 
-                Vector3 rvec = new Vector3((float)rvecs[i * 3], (float)rvecs[i * 3 + 1], (float)rvecs[i * 3 + 2]);
+                Vector3 rvec = new Vector3((float)ArucoTracking.rvecs[i * 3], (float)ArucoTracking.rvecs[i * 3 + 1], (float)ArucoTracking.rvecs[i * 3 + 2]);
                
                 float theta = rvec.magnitude;
                 rvec.Normalize();
@@ -239,9 +183,4 @@ public class aruco_tracker : MonoBehaviour {
             }
         }
 	}
-
-    static void PluginDebugLog(string str)
-    {
-        Debug.Log("Aruco Plugin: " + str);
-    }
 }
