9a324482e7348e11daa9865a639eef4009905647
Tilman Schmidt <AccessKeyMaster@gmx.net>
2017-03-10 17:50:40 +0000

    Added voice input to adjust pose offset. Untested!
    This allows me to adjust the marker pose offset via "left, right, up,
    down, forwards, backwards" voice commands. This should speed up
    iteration on finding the right value quickly.
    The code is as yet untested, since I don't have a hololens around right
    now. However it is pretty much the microsoft example code so it should
    hopefully work just fine.

---
+++ MarkerTracking/aruco_plugin_test/Assets/Scripts/OffsetPhraseRecogniser.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Windows.Speech;
+using System.Linq;
+
+public class OffsetPhraseRecogniser : MonoBehaviour {
+    KeywordRecognizer keywordRecognizer;
+    Dictionary<string, System.Action> keywords = new Dictionary<string, System.Action>();
+
+    public OffsetArucoRunner offsetRunner; //Reference the runner so we can adjust the offset from callbacks
+	// Use this for initialization
+	void Start () {
+        keywords.Add("left", () => {
+            offsetRunner.offset.x -= 0.005f;
+        });
+        keywords.Add("right", () => {
+            offsetRunner.offset.x += 0.005f;
+        });
+        keywords.Add("up", () => {
+            offsetRunner.offset.y += 0.005f;
+        });
+        keywords.Add("down", () => {
+            offsetRunner.offset.y -= 0.005f;
+        });
+        keywords.Add("forwards", () => {
+            offsetRunner.offset.z += 0.005f;
+        });
+        keywords.Add("backwards", () => {
+            offsetRunner.offset.z -= 0.005f;
+        });
+
+        keywordRecognizer = new KeywordRecognizer(keywords.Keys.ToArray());
+
+        keywordRecognizer.OnPhraseRecognized += onPhraseRecognized;
+
+        keywordRecognizer.Start();
+    }
+
+    private void onPhraseRecognized(PhraseRecognizedEventArgs args) {
+        System.Action keywordAction;
+        if(keywords.TryGetValue(args.text, out keywordAction)) {
+            keywordAction.Invoke();
+        }
+    }
+}
