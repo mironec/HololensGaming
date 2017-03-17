517c960a57cc1564034f1bd7e52ed19d47cf3f33
Miron Zelina <mironzelinajr@gmail.com>
2017-03-17 14:06:26 +0000

    Add GravityShifter script
    Shifts gravity if the rigidbody attached does not useGravity.

---
+++ HoloBallGame/Assets/Scripts/GravityShifter.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof (Rigidbody))]
+public class GravityShifter : MonoBehaviour {
+
+    public Vector3 gravityDirection = Vector3.down;
+    public float magnitude = 9.81f;
+
+	void Start () {
+	}
+	
+	void FixedUpdate () {
+        Rigidbody rigidbody = GetComponent<Rigidbody>();
+        if (!rigidbody.useGravity) {
+            rigidbody.AddForce(gravityDirection.normalized * magnitude * rigidbody.mass, ForceMode.Force);
+        }
+	}
+}
