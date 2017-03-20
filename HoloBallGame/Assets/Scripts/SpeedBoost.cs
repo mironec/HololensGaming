bb9ffc11202fad3a1dc2b4dd0f890252915a3236
Miron Zelina <mironzelinajr@gmail.com>
2017-03-20 22:07:33 +0000

    Add direction change to Speed Boost script
    Also add removeVelocity boolean for removing velocity from entering
    objects before boosting them. Adjusts scenes to account for these changes.

--- HoloBallGame/Assets/Scripts/SpeedBoost.cs
+++ HoloBallGame/Assets/Scripts/SpeedBoost.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpeedBoost : MonoBehaviour {
 
     public float speedBoostStrength = 1.0f;
+    public Vector3 speedBoostDirectionRelative;
+    public bool removeVelocity = false;
 
 	// Use this for initialization
 	void Start () {
@@ -20,8 +22,14 @@ public class SpeedBoost : MonoBehaviour {
     {
         Rigidbody rigidbody = other.gameObject.GetComponent<Rigidbody>();
         if (rigidbody == null) return;
-        rigidbody.velocity = Vector3.zero;
-        rigidbody.AddForce(transform.forward * speedBoostStrength, ForceMode.VelocityChange);
-        Debug.Log(other.gameObject + " Entered!");
+        if(removeVelocity)
+            rigidbody.velocity = Vector3.zero;
+        Vector3 direction = transform.forward;
+        if (speedBoostDirectionRelative != null) {
+            direction = Quaternion.AngleAxis(speedBoostDirectionRelative.x, Vector3.right) * direction;
+            direction = Quaternion.AngleAxis(speedBoostDirectionRelative.y, Vector3.up) * direction;
+            direction = Quaternion.AngleAxis(speedBoostDirectionRelative.z, Vector3.forward) * direction;
+        }
+        rigidbody.AddForce(direction * speedBoostStrength, ForceMode.VelocityChange);
     }
 }
