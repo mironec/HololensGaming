8717a3f2654d0379f521c426f3f977378d9a199d
Mehul <mehul1996@hotmail.com>
2017-04-05 01:38:53 +0100

    new version of gui with button and levels selection, to be merged with game

---
+++ GUIv2/Assets/Scripts/Menu.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Menu : MonoBehaviour {
+
+    public Canvas MainCanvas;
+    public Canvas LevelCanvas;
+
+    private void Awake()
+    {
+        LevelCanvas.gameObject.SetActive(false);
+    }
+
+    public void LevelOn()
+    {
+        LevelCanvas.gameObject.SetActive(true);
+        MainCanvas.gameObject.SetActive(false);
+
+    }
+
+    public void ReturnOn()
+    {
+        LevelCanvas.gameObject.SetActive(false);
+        MainCanvas.gameObject.SetActive(true);
+    }
+
+    public void CloseOn()
+    {
+        MainCanvas.gameObject.SetActive(false);
+    }
+
+    public void TerminateOn()
+    {
+        Application.Quit();
+    }
+/*
+    public void LoadOn()
+    {
+        Application.LoadLevel(); 
+    }
+    */
+}
