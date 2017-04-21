ae587874696c00c49da35732a460a6e1a2299aab
Miron Zelina <mironzelinajr@gmail.com>
2017-04-21 23:44:26 +0100

    Add play on tap to the playball
    Also pauses the game if you manage to catch the ball through some miracle.

---
+++ HoloBallGame/Assets/Scripts/OnClickInteractible.cs
@@ -0,0 +1,25 @@
+﻿using HoloToolkit.Unity.InputModule;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+
+public class OnClickInteractible : MonoBehaviour, IInputClickHandler {
+
+    public UnityEvent onClick;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        if(enabled)
+            onClick.Invoke();
+    }
+
+    void Start () {
+		
+	}
+	
+	void Update () {
+		
+	}
+}
